4d3f3a14ea5a9ab1757c2503f18e72d0e3b64be1
ailadonayre <23-02175@g.batstate-u.edu.ph>
2024-12-16 02:06:18 +0800

    Fixed MySQL queries

--- src/agent/ArkaAgentManager.java
+++ src/agent/ArkaAgentManager.java
@@ -22,7 +22,7 @@ public class ArkaAgentManager {
     public void signUp() {
         String username = null;
         String password = null;
-
+    
         try {
             System.out.println(ArkaCustom.ANSI_BOLD + "\n------------------------------------------------------------------------------------------\n" + ArkaCustom.ANSI_RESET);
             System.out.println(ArkaCustom.ANSI_BOLD + ArkaCustom.ANSI_PURPLE + "ARKA: " + ArkaCustom.ANSI_RESET + ArkaCustom.ANSI_PURPLE + "Create an account" + ArkaCustom.ANSI_RESET);
@@ -35,7 +35,7 @@ public class ArkaAgentManager {
             if (username.length() < 4) {
                 throw new IllegalArgumentException("Username must be at least 4 characters long.");
             }
-
+    
             System.out.print(ArkaCustom.ANSI_BOLD + ArkaCustom.ANSI_PURPLE + "> " + ArkaCustom.ANSI_RESET);
             System.out.print("Enter password (must have at least eight characters): ");
             password = scanner.nextLine();
@@ -45,7 +45,7 @@ public class ArkaAgentManager {
             if (password.length() < 8) {
                 throw new IllegalArgumentException("Password must be at least 8 characters long.");
             }
-
+    
             try (Connection conn = ArkaDatabase.getConnection()) {
                 String checkSql = "SELECT COUNT(*) FROM agent WHERE username = ?";
                 try (PreparedStatement checkStatement = conn.prepareStatement(checkSql)) {
@@ -57,20 +57,21 @@ public class ArkaAgentManager {
                         return;
                     }
                 }
-
+    
                 String agentID = generateAgentID();
                 String sql = "INSERT INTO agent (agentID, username, password) VALUES (?, ?, ?)";
                 try (PreparedStatement statement = conn.prepareStatement(sql)) {
                     statement.setString(1, agentID);
                     statement.setString(2, username);
                     statement.setString(3, password);
-
+    
                     int rowsInserted = statement.executeUpdate();
                     if (rowsInserted > 0) {
                         System.out.print(ArkaCustom.ANSI_BOLD + ArkaCustom.ANSI_CYAN + "\t>> " + ArkaCustom.ANSI_RESET);
                         System.out.println("You have successfully signed up!");
                         System.out.print(ArkaCustom.ANSI_BOLD + ArkaCustom.ANSI_CYAN + "\t>> " + ArkaCustom.ANSI_RESET);
                         System.out.println("Agent ID: " + ArkaCustom.ANSI_BOLD + agentID + ArkaCustom.ANSI_RESET);
+    
                     }
                 } catch (SQLException e) {
                     System.out.print(ArkaCustom.ANSI_BOLD + ArkaCustom.ANSI_YELLOW + "\t>> " + ArkaCustom.ANSI_RESET);
@@ -82,7 +83,7 @@ public class ArkaAgentManager {
                 System.out.println("Error establishing a database connection during sign-up.");
                 e.printStackTrace();
             }
-
+    
         } catch (IllegalArgumentException e) {
             System.out.print(ArkaCustom.ANSI_BOLD + ArkaCustom.ANSI_YELLOW + "\t>> " + ArkaCustom.ANSI_RESET);
             System.out.println("Error: " + e.getMessage());
@@ -91,7 +92,7 @@ public class ArkaAgentManager {
             System.out.println("An unexpected error occurred during sign-up.");
             e.printStackTrace();
         }
-    }
+    }    
 
     public boolean signIn() {
         String identifier = null;
--- src/db/ArkaDatabase.java
+++ src/db/ArkaDatabase.java
@@ -6,6 +6,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.time.LocalDate;
 
 import models.ArkaGintoPlan;
@@ -37,6 +38,162 @@ public class ArkaDatabase {
             System.out.println("Error connecting to the database.");
             e.printStackTrace();
             throw e;
+        }
+    }
+    
+    public void initializeDatabase() {
+        try (Connection connection = getConnection()) {
+            checkAndCreateTables(connection);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private boolean tableExists(Connection connection, String tableName) throws SQLException {
+        String query = "SHOW TABLES LIKE ?";
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setString(1, tableName);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                return resultSet.next();
+            }
+        }
+    }
+
+    private void checkAndCreateTables(Connection connection) throws SQLException {
+        if (!tableExists(connection, "agent")) {
+            createAgentTable();
+        }
+        if (!tableExists(connection, "client")) {
+            createClientTable();
+        }
+        if (!tableExists(connection, "policy")) {
+            createPolicyTable();
+        }
+        if (!tableExists(connection, "payment")) {
+            createPaymentTable();
+        }
+        if (!tableExists(connection, "address")) {
+            createAddressTable();
+        }
+        if (!tableExists(connection, "citizenship")) {
+            createCitizenshipTable();
+        }
+        if (!tableExists(connection, "income")) {
+            createIncomeTable();
+        }
+    }
+    
+    private void createAgentTable() throws SQLException {
+        String createAgentTableSQL = "CREATE TABLE `agent` ("
+                + "`agentID` varchar(10) NOT NULL, "
+                + "`username` varchar(255) NOT NULL, "
+                + "`password` varchar(255) NOT NULL, "
+                + "PRIMARY KEY (`agentID`), "
+                + "UNIQUE KEY `username` (`username`)"
+                + ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci";
+        executeSQL(createAgentTableSQL);
+    }
+    
+    private void createClientTable() throws SQLException {
+        String createClientTableSQL = "CREATE TABLE `client` ("
+                + "`clientID` varchar(10) NOT NULL, "
+                + "`firstName` varchar(100) NOT NULL, "
+                + "`lastName` varchar(100) NOT NULL, "
+                + "`email` varchar(255) NOT NULL, "
+                + "`province` varchar(100) NOT NULL, "
+                + "`city` varchar(100) NOT NULL, "
+                + "`barangay` varchar(100) DEFAULT NULL, "
+                + "`street` varchar(100) DEFAULT NULL, "
+                + "PRIMARY KEY (`clientID`)"
+                + ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci";
+        executeSQL(createClientTableSQL);
+    }
+    
+    private void createPolicyTable() throws SQLException {
+        String createPolicyTableSQL = "CREATE TABLE `policy` ("
+                + "`policyID` varchar(20) NOT NULL, "
+                + "`clientID` varchar(10) NOT NULL, "
+                + "`policyType` varchar(50) NOT NULL, "
+                + "`coverageAmount` decimal(15,2) NOT NULL, "
+                + "`policyDate` date DEFAULT NULL, "
+                + "`policyEndDate` date DEFAULT NULL, "
+                + "`beneficiaries` varchar(255) NOT NULL, "
+                + "`agentID` varchar(10) NOT NULL, "
+                + "PRIMARY KEY (`policyID`), "
+                + "KEY `clientID` (`clientID`), "
+                + "KEY `fk_policy_agentID` (`agentID`), "
+                + "CONSTRAINT `fk_policy_agentID` FOREIGN KEY (`agentID`) REFERENCES `agent` (`agentID`) ON DELETE CASCADE, "
+                + "CONSTRAINT `policy_ibfk_1` FOREIGN KEY (`clientID`) REFERENCES `client` (`clientID`) ON DELETE CASCADE"
+                + ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci";
+        executeSQL(createPolicyTableSQL);
+    }
+    
+    private void createPaymentTable() throws SQLException {
+        String createPaymentTableSQL = "CREATE TABLE `payment` ("
+                + "`paymentID` int NOT NULL AUTO_INCREMENT, "
+                + "`clientID` varchar(10) NOT NULL, "
+                + "`agentID` varchar(10) NOT NULL, "
+                + "`policyID` varchar(20) NOT NULL, "
+                + "`paymentDate` date DEFAULT NULL, "
+                + "`paymentAmount` decimal(15,2) NOT NULL, "
+                + "`nextPayment` date DEFAULT NULL, "
+                + "`lastPayment` date DEFAULT NULL, "
+                + "PRIMARY KEY (`paymentID`), "
+                + "KEY `clientID` (`clientID`), "
+                + "KEY `policyID` (`policyID`), "
+                + "KEY `fk_payment_agentID` (`agentID`), "
+                + "CONSTRAINT `fk_payment_agentID` FOREIGN KEY (`agentID`) REFERENCES `agent` (`agentID`) ON DELETE CASCADE, "
+                + "CONSTRAINT `payment_ibfk_1` FOREIGN KEY (`clientID`) REFERENCES `client` (`clientID`) ON DELETE CASCADE, "
+                + "CONSTRAINT `payment_ibfk_2` FOREIGN KEY (`policyID`) REFERENCES `policy` (`policyID`) ON DELETE CASCADE"
+                + ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci";
+        executeSQL(createPaymentTableSQL);
+    }
+    
+    private void createAddressTable() throws SQLException {
+        String createAddressTableSQL = "CREATE TABLE `address` ("
+                + "`addressID` int NOT NULL AUTO_INCREMENT, "
+                + "`clientID` varchar(10) NOT NULL, "
+                + "`country` varchar(100) NOT NULL, "
+                + "`province` varchar(100) NOT NULL, "
+                + "`city` varchar(100) NOT NULL, "
+                + "`barangay` varchar(100) DEFAULT NULL, "
+                + "`street` varchar(100) DEFAULT NULL, "
+                + "PRIMARY KEY (`addressID`), "
+                + "KEY `fk_clientID_address` (`clientID`), "
+                + "CONSTRAINT `address_ibfk_1` FOREIGN KEY (`clientID`) REFERENCES `client` (`clientID`) ON DELETE CASCADE"
+                + ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci";
+        executeSQL(createAddressTableSQL);
+    }
+    
+    private void createCitizenshipTable() throws SQLException {
+        String createCitizenshipTableSQL = "CREATE TABLE `citizenship` ("
+                + "`citizenshipID` int NOT NULL AUTO_INCREMENT, "
+                + "`clientID` varchar(10) NOT NULL, "
+                + "`citizenship` varchar(100) NOT NULL, "
+                + "`nationality` varchar(100) NOT NULL, "
+                + "PRIMARY KEY (`citizenshipID`), "
+                + "KEY `fk_clientID_citizenship` (`clientID`), "
+                + "CONSTRAINT `citizenship_ibfk_1` FOREIGN KEY (`clientID`) REFERENCES `client` (`clientID`) ON DELETE CASCADE"
+                + ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci";
+        executeSQL(createCitizenshipTableSQL);
+    }
+    
+    private void createIncomeTable() throws SQLException {
+        String createIncomeTableSQL = "CREATE TABLE `income` ("
+                + "`incomeID` int NOT NULL AUTO_INCREMENT, "
+                + "`clientID` varchar(10) NOT NULL, "
+                + "`sourceIncome` varchar(100) NOT NULL, "
+                + "`annualIncome` int NOT NULL, "
+                + "PRIMARY KEY (`incomeID`), "
+                + "KEY `fk_clientID_income` (`clientID`), "
+                + "CONSTRAINT `income_ibfk_1` FOREIGN KEY (`clientID`) REFERENCES `client` (`clientID`) ON DELETE CASCADE"
+                + ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci";
+        executeSQL(createIncomeTableSQL);
+    }
+
+    private void executeSQL(String query) throws SQLException {
+        try (Connection connection = getConnection(); Statement statement = connection.createStatement()) {
+            statement.executeUpdate(query);
         }
     }
 
